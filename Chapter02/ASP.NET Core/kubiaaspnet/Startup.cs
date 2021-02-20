f0f7f69fd38e8bfa639b4a816f574b5d3fb7dade
Owain Williams <owain@hedfan.uk>
2021-02-20 14:48:59 +0000

    Log request details and output info about the connection.

--- Chapter02/ASP.NET Core/kubiaaspnet/Startup.cs
+++ Chapter02/ASP.NET Core/kubiaaspnet/Startup.cs
@@ -1,39 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace kubiaaspnet
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app, ILogger<Startup> logger)
         {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            app.Run(async context =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                logger.LogInformation($"Received request for {context.Request.GetDisplayUrl()} from {context.Connection.RemoteIpAddress}");
+                await context.Response.WriteAsync($"Hey there, this is {Environment.MachineName}. Your IP is {context.Connection.RemoteIpAddress}.\n");
             });
         }
     }
